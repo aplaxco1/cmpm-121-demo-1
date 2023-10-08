33e8f9d769fc30f75c9118220f5c69f646dcc7d3
aplaxco1 <aplaxco@ucsc.edu>
2023-10-08 09:36:01 -0700

    step 2 complete

--- src/main.ts
+++ src/main.ts
@@ -15,3 +15,15 @@ button.className = "mainButton";
 button.type = "button";
 button.innerHTML = "☕";
 app.append(button);
+
+let counter: number = 0;
+
+const counterText = document.createElement("div");
+counterText.className = "buttonText";
+counterText.innerHTML = `${counter} Cups of Coffee`;
+app.append(counterText);
+
+app.addEventListener("click", function () {
+  counter += 1;
+  counterText.innerHTML = `${counter} Cups of Coffee`;
+});
