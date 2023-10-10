4bac00787e5dc54ce42a051cb23e36f7810bae3e
aplaxco1 <aplaxco@ucsc.edu>
2023-10-10 11:18:06 -0700

    step 7 complete

--- src/main.ts
+++ src/main.ts
@@ -45,32 +45,38 @@ function automaticIncrement(timeStamp: number) {
 
 const buttons = document.createElement("container");
 
-const upgrade1Cost = 10;
+let upgrade1Cost = 10;
 const upgrade1Rate = 0.1;
 let upgrade1Purchased = 0;
-const upgrade2Cost = 100;
+let upgrade2Cost = 100;
 const upgrade2Rate = 2;
 let upgrade2Purchased = 0;
-const upgrade3Cost = 1000;
+let upgrade3Cost = 1000;
 const upgrade3Rate = 50;
 let upgrade3Purchased = 0;
 
 const upgradeButton1 = document.createElement("button");
 upgradeButton1.className = "upgradeButton";
 upgradeButton1.type = "button";
-upgradeButton1.innerHTML = `Upgrade A: ${upgrade1Rate} Cups/Second<br>Cost: ${upgrade1Cost} | Purchased: ${upgrade1Purchased}`;
+upgradeButton1.innerHTML = `Upgrade A: ${upgrade1Rate} Cups/Second<br>Cost: ${upgrade1Cost.toFixed(
+  2,
+)} | Purchased: ${upgrade1Purchased}`;
 upgradeButton1.disabled = true;
 
 const upgradeButton2 = document.createElement("button");
 upgradeButton2.className = "upgradeButton";
 upgradeButton2.type = "button";
-upgradeButton2.innerHTML = `Upgrade B: ${upgrade2Rate} Cups/Second<br>Cost: ${upgrade2Cost} | Purchased: ${upgrade2Purchased}`;
+upgradeButton2.innerHTML = `Upgrade B: ${upgrade2Rate} Cups/Second<br>Cost: ${upgrade2Cost.toFixed(
+  2,
+)} | Purchased: ${upgrade2Purchased}`;
 upgradeButton2.disabled = true;
 
 const upgradeButton3 = document.createElement("button");
 upgradeButton3.className = "upgradeButton";
 upgradeButton3.type = "button";
-upgradeButton3.innerHTML = `Upgrade C: ${upgrade3Rate} Cups/Second<br>Cost: ${upgrade3Cost} | Purchased: ${upgrade3Purchased}`;
+upgradeButton3.innerHTML = `Upgrade C: ${upgrade3Rate} Cups/Second<br>Cost: ${upgrade3Cost.toFixed(
+  2,
+)} | Purchased: ${upgrade3Purchased}`;
 upgradeButton3.disabled = true;
 
 buttons.appendChild(upgradeButton1);
@@ -79,7 +85,9 @@ buttons.appendChild(upgradeButton3);
 app.append(buttons);
 
 function updateUpgrade1Button() {
-  upgradeButton1.innerHTML = `Upgrade A: ${upgrade1Rate} Cups/Second<br>Cost: ${upgrade1Cost} | Purchased: ${upgrade1Purchased}`;
+  upgradeButton1.innerHTML = `Upgrade A: ${upgrade1Rate} Cups/Second<br>Cost: ${upgrade1Cost.toFixed(
+    2,
+  )} | Purchased: ${upgrade1Purchased}`;
   if (counter >= upgrade1Cost) {
     upgradeButton1.disabled = false;
   } else {
@@ -88,7 +96,9 @@ function updateUpgrade1Button() {
 }
 
 function updateUpgrade2Button() {
-  upgradeButton2.innerHTML = `Upgrade B: ${upgrade2Rate} Cups/Second<br>Cost: ${upgrade2Cost} | Purchased: ${upgrade2Purchased}`;
+  upgradeButton2.innerHTML = `Upgrade B: ${upgrade2Rate} Cups/Second<br>Cost: ${upgrade2Cost.toFixed(
+    2,
+  )} | Purchased: ${upgrade2Purchased}`;
   if (counter >= upgrade2Cost) {
     upgradeButton2.disabled = false;
   } else {
@@ -97,7 +107,9 @@ function updateUpgrade2Button() {
 }
 
 function updateUpgrade3Button() {
-  upgradeButton3.innerHTML = `Upgrade C: ${upgrade3Rate} Cups/Second<br>Cost: ${upgrade3Cost} | Purchased: ${upgrade3Purchased}`;
+  upgradeButton3.innerHTML = `Upgrade C: ${upgrade3Rate} Cups/Second<br>Cost: ${upgrade3Cost.toFixed(
+    2,
+  )} | Purchased: ${upgrade3Purchased}`;
   if (counter >= upgrade3Cost) {
     upgradeButton3.disabled = false;
   } else {
@@ -109,6 +121,7 @@ upgradeButton1.addEventListener("click", function () {
   if (counter >= upgrade1Cost) {
     counter -= upgrade1Cost;
     rateOfIncrease += upgrade1Rate;
+    upgrade1Cost = priceIncrease(upgrade1Cost);
     upgrade1Purchased += 1;
   }
 });
@@ -117,6 +130,7 @@ upgradeButton2.addEventListener("click", function () {
   if (counter >= upgrade2Cost) {
     counter -= upgrade2Cost;
     rateOfIncrease += upgrade2Rate;
+    upgrade2Cost = priceIncrease(upgrade2Cost);
     upgrade2Purchased += 1;
   }
 });
@@ -125,6 +139,7 @@ upgradeButton3.addEventListener("click", function () {
   if (counter >= upgrade3Cost) {
     counter -= upgrade3Cost;
     rateOfIncrease += upgrade3Rate;
+    upgrade3Cost = priceIncrease(upgrade3Cost);
     upgrade3Purchased += 1;
   }
 });
@@ -135,6 +150,10 @@ currentRateText.innerHTML = `${rateOfIncrease.toFixed(
   1,
 )} Cups of Coffee per Second`;
 app.append(currentRateText);
+
+function priceIncrease(price: number): number {
+  return price * 1.15;
+}
 
 // main animation loop
 function update() {
